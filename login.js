d384c3375e4c275a329be7770ec2c8a0b4c35150
K <karaleash462@gmail.com>
2022-02-07 11:28:25 +0530

    first commit

---
+++ login.js
@@ -0,0 +1,78 @@
+
+import { GoogleAuthProvider } from "firebase/auth";
+
+let provider = new GoogleAuthProvider();
+import { getAuth, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
+
+const auth = getAuth();
+getRedirectResult(auth)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access Google APIs.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+
+    // The signed-in user info.
+    const user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+
+(function () {
+    
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: YOUR-API-KEY-HERE
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    //Get Elements
+    const txtEmail = document.getElementById("txtEmail");
+    const txtPassword = document.getElementById("txtPassword");
+    const btnLogin = document.getElementById("btnLogin");
+    const btnSignup = document.getElementById("btnSignup");
+
+    //Add Login Event
+    btnLogin.addEventListener('click', e => {
+        const email = txtEmail.value;
+        const password = txtPassword.value;
+
+        const auth = firebase.auth();
+
+        //sign in with firebase auth
+        auth.signInWithEmailAndPassword(email, password).then(user =>{
+            alert("Login Successful :)");
+        }).catch(err => {
+            alert(err.message);
+        });
+        
+    });
+
+    //Add Signup Event
+    btnSignup.addEventListener('click', e => {
+
+        //get email and password
+        const email = txtEmail.value;
+        const password = txtPassword.value;
+
+        const auth = firebase.auth();
+
+        //sign in with firebase auth
+        const promise = auth.createUserWithEmailAndPassword(email, password).then(user => {
+            alert("Signup Successful :)")
+        }).catch(err => {
+            alert(err.message);
+        });
+
+    });
+
+
+}());
